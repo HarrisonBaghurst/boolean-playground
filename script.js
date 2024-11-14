59b1a6b5e817142ca198d4576492a282cf60b4fd
Harrison Baghurst <070806harrison@gmail.com>
2024-11-14 15:53:43 +0000

    added svg image for and gate

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ class MovableDiv {
         this.div.id = this;
         this.div.className = 'movable'; 
         this.xOffset = 0; 
-        this.yOffset = 0; 
+        this.yOffset = 0;
+        this.currentDivType = null;
         document.body.appendChild(this.div); 
         this.div.addEventListener('click', () => this.clickedDiv());
 
@@ -19,33 +20,44 @@ class MovableDiv {
         this.output = document.createElement('div'); 
         this.output.className = 'output';
         this.div.appendChild(this.output); 
+        this.output.addEventListener('click', (event) => this.outputClicked(event))
 
         // creating input divs 
         this.input1 = document.createElement('div');
         this.input1.className = 'input'; 
         this.div.appendChild(this.input1); 
+        this.input1.addEventListener('click', function(event) {
+            currentDiv = this.input1; 
+            event.stopPropagation();
+        })
         this.input2 = document.createElement('div'); 
         this.input2.className = 'input'; 
         this.input2.style.top = '3vw'; 
         this.div.appendChild(this.input2); 
+        this.input2.addEventListener('click', function(event) {
+            currentDiv = this.input2; 
+            event.stopPropagation();
+        })
     }
 
     // if div clicked, set current if not already current else set not current 
     clickedDiv() {
-        if (this === currentDiv) { 
+        if (this === currentDiv && this.currentDivType === 'main div') { 
             this.div.style.backgroundColor = "rgb(150, 150, 150)";
             // if already current div and top bar clicked delete object 
             if (yPos < document.getElementById('top-bar').getBoundingClientRect()['height']) { 
                 this.div.parentNode.removeChild(this.div); 
             }
             currentDiv = null; 
+            this.currentDivType = null; 
         } 
-        else if (this != currentDiv) { 
+        else if (this != currentDiv || this.currentDivType != 'main div') { 
             currentDiv = this; 
             this.div.style.backgroundColor = "rgb(0, 150, 150)";
             var rect = this.div.getBoundingClientRect(); 
             this.xOffset = xPos - rect['x']; 
             this.yOffset = yPos - rect['y']; 
+            this.currentDivType = 'main div' 
         }
     }
 
@@ -53,6 +65,19 @@ class MovableDiv {
     setDivPosition() { 
         this.div.style.left = xPos - this.xOffset + 'px'; 
         this.div.style.top = yPos - this.yOffset + 'px'; 
+    }
+
+    // get type of div
+    getDivType() {
+        return this.currentDivType;
+    }
+
+    // on output div clicked 
+    outputClicked(event) {
+        currentDiv = this; 
+        this.currentDivType = 'output div'
+        event.stopPropagation();
+        console.log(currentDiv, this.currentDivType)
     }
 }
 
@@ -68,11 +93,10 @@ window.addEventListener("DOMContentLoaded", () => {
     function setMousePosition(e) { 
         xPos = e.clientX;
         yPos = e.clientY;
-        if (currentDiv != null) { 
+        if (currentDiv != null && currentDiv.getDivType() === 'main div') { 
             currentDiv.setDivPosition(); 
         }
     }
 
     document.body.addEventListener("mousemove", setMousePosition);
-
 })
